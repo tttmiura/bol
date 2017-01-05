6032b368ef14378028da15ad9ccc0742f1a2e096
MIURA <takashi.miura@soliton.co.jp>
2017-01-05 14:57:02 +0900

    必要そうなlib追加

---
+++ public/js/index.js
@@ -0,0 +1,32 @@
+/*
+ * index.js
+ */
+'use strict';
+Reveal.initialize({
+	keyboard: {
+		13: 'next', // Enter
+		68: 'down', // d
+		78: 'next', // n
+		80: 'prev', // p
+		85: 'up' // u
+	},
+	controls: true,
+	progress: true,
+	history: false,
+	center: true,
+
+	transition: 'convex', // none/fade/slide/convex/concave/zoom
+
+	rollingLinks: true,
+
+	// Optional reveal.js plugins
+	dependencies: [
+		{ src: 'public/lib/js/classList.js', condition: function() { return !document.body.classList; } },
+		{ src: 'public/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+		{ src: 'public/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+		{ src: 'public/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+		{ src: 'public/plugin/zoom-js/zoom.js', async: true },
+		{ src: 'public/plugin/notes/notes.js', async: true }
+	]
+});
+
